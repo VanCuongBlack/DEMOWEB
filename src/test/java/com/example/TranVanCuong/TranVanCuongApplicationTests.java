6afdce808a1c08d5b23cb07f7d495649d3f84737
VanCuong <vancuong@example.com>
2026-03-19 10:48:04 +0700

    Initial commit with Docker workflow

---
+++ src/test/java/com/example/TranVanCuong/TranVanCuongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TranVanCuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranVanCuongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
